1c1b12b757fb26da13d834a7d651227fd30f0594
Eric Crooks <eric.crooks@gmail.com>
2021-10-19 04:19:05 -0400

    feat: add necessary files

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+module.exports = {
+  publicRuntimeConfig: {
+    versions: {
+      drash: {
+        versions: [
+          "v1.x",
+          "v2.x",
+        ]
+      }
+    }
+  },
+  reactStrictMode: false,
+};
