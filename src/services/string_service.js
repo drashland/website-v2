c48428ee300d185fc322cef987a4cffdb40cc438
Eric Crooks <eric.crooks@gmail.com>
2021-11-17 21:40:51 -0500

    chore: alphabetize array

--- src/services/string_service.js
+++ src/services/string_service.js
@@ -9,6 +9,18 @@
 export function formatLabel(label) {
   const replacements = [
     {
+      from: /Api/g,
+      to: "API"
+    },
+    {
+      from: /Cors/g,
+      to: "CORS"
+    },
+    {
+      from: /Csrf/g,
+      to: "CSRF"
+    },
+    {
       from: /Etag/g,
       to: "ETag"
     },
@@ -29,36 +41,24 @@ export function formatLabel(label) {
       to: "JSON"
     },
     {
-      from: /Url/g,
-      to: "URL"
+      from: /Resource Level/g,
+      to: "Resource-Level"
     },
     {
       from: /(Server Level)/g,
       to: "Server-Level"
     },
     {
-      from: /Resource Level/g,
-      to: "Resource-Level"
-    },
-    {
-      from: /Cors/g,
-      to: "CORS"
-    },
-    {
-      from: /Csrf/g,
-      to: "CSRF"
-    },
-    {
-      from: /Api/g,
-      to: "API"
+      from: /Spa/g,
+      to: "SPA"
     },
     {
       from: /Ssr/g,
       to: "SSR"
     },
     {
-      from: /Spa/g,
-      to: "SPA"
+      from: /Url/g,
+      to: "URL"
     },
     {
       from: /V2/g,
