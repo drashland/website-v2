3dbaf879d72ab7e483b81e2383ac746386acdbb2
Eric Crooks <eric.crooks@gmail.com>
2022-07-03 01:03:04 -0400

    chore: deno fmt

--- src/components/placeholders/CreateDepsFileStepTextOnly.jsx
+++ src/components/placeholders/CreateDepsFileStepTextOnly.jsx
@@ -4,7 +4,16 @@ export default function CreateDepsFileStepTextOnly() {
   return (
     <>
       <Paragraph>
-        Create your <Code>deps.ts</Code> file. Replace <Code>&lt;VERSION&gt;</Code> with the latest version of Drash v2.x. The latest version can be found at <a href="https://github.com/drashland/drash/releases/latest" target="_BLANK">https://github.com/drashland/drash/releases/latest</a>.
+        Create your <Code>deps.ts</Code> file. Replace{" "}
+        <Code>&lt;VERSION&gt;</Code>{" "}
+        with the latest version of Drash v2.x. The latest version can be found
+        at{" "}
+        <a
+          href="https://github.com/drashland/drash/releases/latest"
+          target="_BLANK"
+        >
+          https://github.com/drashland/drash/releases/latest
+        </a>.
       </Paragraph>
     </>
   );
