d0807078bfe1ae95679dff80bc64f8edff15f373
Eric Crooks <crookse@users.noreply.github.com>
2023-11-04 10:32:03 -0400

    fix: discord links

--- src/components/nextra/LinkExternalDiscord.tsx
+++ src/components/nextra/LinkExternalDiscord.tsx
@@ -3,7 +3,7 @@ import { IconLinkExternal } from "./IconLinkExternal";
 export const LinkExternalDiscord = () => (
   <a
     className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]"
-    href="https://discord.gg/RhYs6zQJ"
+    href="https://discord.gg/UuYKTVMW"
     target="_blank"
   >
     Discord<span className="link-external-arrow inline-block">
